26c3ad9d7efd05be01e398f3f6d7700e9489103a
Vanessa Dean <vanessa.dean@gmail.com>
2014-07-28 13:05:21 -0400

    Style changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   resources :tasks
   resources :lists
 
-  root 'lists#index'
+  root 'lists#new'
+  post '/lists' => 'lists#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
